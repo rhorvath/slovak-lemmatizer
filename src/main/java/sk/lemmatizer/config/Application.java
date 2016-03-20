71eca35b7b5315d56bc4b3ab41e84c0dc05d1fb4
Robert Horvath <roberthorvath89@gmail.com>
2016-03-20 23:35:21 +0100

    spring boot application config

---
+++ src/main/java/sk/lemmatizer/config/Application.java
@@ -0,0 +1,25 @@
+package sk.lemmatizer.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("sk.lemmatizer")
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
