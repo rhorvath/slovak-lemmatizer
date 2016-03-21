2eb813c8727b9fcbf34a3e8d5e1bd246f2a160c6
Robert Horvath <roberthorvath89@gmail.com>
2016-03-22 00:30:56 +0100

    added entity scan and enabled jpa repositories

--- src/main/java/sk/lemmatizer/config/Application.java
+++ src/main/java/sk/lemmatizer/config/Application.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan("sk.lemmatizer")
+@EntityScan("sk.lemmatizer.model")
+@EnableJpaRepositories("sk.lemmatizer.dao")
 public class Application extends SpringBootServletInitializer {
 
     @Override
