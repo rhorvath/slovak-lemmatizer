0069e62e8b1dc0aba7cbd08d9c1fcabe71a9f306
Robert Horvath <roberthorvath89@gmail.com>
2016-03-23 08:32:36 +0100

    fast lemmatisation implemented (without tvaroslovnik)

--- src/main/java/sk/lemmatizer/service/LemmatizationService.java
+++ src/main/java/sk/lemmatizer/service/LemmatizationService.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sk.lemmatizer.dao.WordDao;
 import sk.lemmatizer.model.Word;
+import sk.lemmatizer.utils.TextUtils;
 
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class LemmatizationService {
@@ -20,11 +23,45 @@ public class LemmatizationService {
     private WordDao wordDao;
 
     public String fastLemmatize(String text, boolean useDatabase, boolean useTvaroslovnik, boolean inputDiacritics, boolean outputDiacritics, boolean keepStructure) {
-        throw new UnsupportedOperationException("not implemented");
+        StringBuffer stringBuffer = new StringBuffer();
+        if (keepStructure) {
+            Matcher matcher = Pattern.compile("(\\p{L})+").matcher(text);
+            while (matcher.find()) {
+                matcher.appendReplacement(stringBuffer, findSingleLemma(matcher.group(), useDatabase, useTvaroslovnik, inputDiacritics));
+            }
+            matcher.appendTail(stringBuffer);
+        } else {
+            String words[] = TextUtils.removeNonAlpha(text.toLowerCase()).split("\\s");
+            for (int i = 0; i < words.length; i++) {
+                stringBuffer.append(findSingleLemma(words[i], useDatabase, useTvaroslovnik, inputDiacritics));
+                if (i < words.length - 1) {
+                    stringBuffer.append(" ");
+                }
+            }
+        }
+        return outputDiacritics ? stringBuffer.toString() : TextUtils.deAccent(stringBuffer.toString());
     }
 
     public List<List<Word>> fullLemmatize(String text, boolean useDatabase, boolean useTvaroslovnik, boolean inputDiacritics, boolean outputDiacritics, boolean keepStructure) {
         throw new UnsupportedOperationException("not implemented");
     }
+
+    private String findSingleLemma(String word, boolean useDatabase, boolean useTvaroslovnik, boolean inputDiacritics) {
+        String lemma = null;
+        if (useDatabase) {
+            List<Word> wordList = wordDao.findByForm(word);
+            if (wordList.isEmpty() && !inputDiacritics) {
+                wordList = wordDao.findByFormAi(word);
+            }
+            if (!wordList.isEmpty()) {
+                lemma = wordList.get(0).getLemma();
+            }
+        }
+        if (lemma == null && useTvaroslovnik) {
+            //TODO: add tvaroslovnik implementation
+        }
+        return lemma != null ? lemma : word;
+    }
+
 
 }
