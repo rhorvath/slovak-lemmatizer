265915ad50f8e61529e29be519f639691954ca6c
Robert Horvath <roberthorvath89@gmail.com>
2016-03-29 23:53:15 +0200

    dictionary import

---
+++ src/main/java/sk/lemmatizer/utils/DictionaryImporter.java
@@ -0,0 +1,49 @@
+package sk.lemmatizer.utils;
+
+import com.opencsv.CSVReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import sk.lemmatizer.dao.WordDao;
+import sk.lemmatizer.model.Word;
+
+import java.io.*;
+
+@Component
+public class DictionaryImporter implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DictionaryImporter.class);
+
+    @Autowired
+    private WordDao wordDao;
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (String arg : args) {
+            if (arg.startsWith("import") && arg.contains("=")) {
+                this.importWords(arg.split("=")[1]);
+            }
+        }
+    }
+
+    private void importWords(String filepath) throws IOException {
+        LOGGER.warn("Starting dictionary import");
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(filepath), "UTF-8");
+        CSVReader reader = new CSVReader(isr, '\t', '\"', 1);
+        String[] row = reader.readNext();
+        while (row != null) {
+            Word word = new Word();
+            word.setLemma(row[0]);
+            word.setForm(row[1]);
+            word.setFormAi(TextUtils.deAccent(row[1]));
+            word.setTag(row[2]);
+            wordDao.save(word);
+            row = reader.readNext();
+        }
+        reader.close();
+        LOGGER.warn("Dictionary import finished");
+    }
+
+}
