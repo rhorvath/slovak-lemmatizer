5fa1d14834e28ca60877ac1c089b7653dc6ee941
Robert Horvath <roberthorvath89@gmail.com>
2016-10-07 19:01:34 +0200

    spring boot version update (1.4.1)

--- src/main/java/sk/lemmatizer/config/Application.java
+++ src/main/java/sk/lemmatizer/config/Application.java
@@ -2,9 +2,9 @@ package sk.lemmatizer.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
